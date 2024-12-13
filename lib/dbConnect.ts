f00989076c1e07cbfc2152cfb6c954e70bb8ff3d
Amit Ramrakhyani <amit.ramrakhyani1109@gmail.com>
2024-12-13 19:11:43 +0530

    Updated site. Added email feature

--- lib/dbConnect.ts
+++ lib/dbConnect.ts
@@ -6,16 +6,14 @@ if (!MONGO_URI) {
   throw new Error("Please define the MONGO_URI environment variable in .env.local");
 }
 
-// Check if global.mongoose exists or not
-let cached = global.mongoose;
+// Initialize the cached object safely
+let cached = global.mongoose as { conn: mongoose.Connection | null, promise: Promise<typeof mongoose> | null } | undefined;
 
-// Ensure that cached is always defined
-if (!cached || !cached.conn) {
+if (!cached) {
   cached = global.mongoose = { conn: null, promise: null };
 }
 
 async function dbConnect() {
-  // Check if cached.conn exists
   if (cached.conn) {
     return cached.conn;
   }
@@ -24,9 +22,7 @@ async function dbConnect() {
     cached.promise = mongoose.connect(MONGO_URI, { bufferCommands: false }).then((mongoose) => mongoose);
   }
 
-  // Ensure cached.conn is set after promise resolves
   cached.conn = await cached.promise;
-
   return cached.conn;
 }
 
